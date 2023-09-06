18ed61aa3d0177a1e5d60606a77d155eaceab35c
Tarun Khurana <khurana.tarun.tk@gmail.com>
2023-09-06 11:48:00 +0530

    more files

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Input = (props) => {
-  const { type, value } = props;
+  const [value, setValue] = useState('')
+  const { type, defaultValue, onChangeEvent } = props;
+
+  useEffect(() => {
+    if(defaultValue) setValue(defaultValue)
+  }, [defaultValue, setValue])
+
+  const onChange = e => {
+    setValue(e.target.value)
+    onChangeEvent(e.target.value)
+  }
   return (
-    <input type={type} value={value} />
+    <input type={type} value={value} onChange={onChange}/>
   )
 }
 
--- src/pages/auth/signin/Signin.jsx
+++ src/pages/auth/signin/Signin.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Input from '../../../components/form/Input'
 
 const Signin = () => {
+ 
+  const onChangeEvent = (value) => {
+    console.log(value)
+  }
   return (
     <div>
       This,is signin page
       <form>
-        <Input type="text" value='Input Value'/>
+        <Input type="text" defaultValue='Input Value' onChangeEvent={onChangeEvent}/>
       </form>
     </div>
   )
